95c357ab45c2bfdcedd6c77b4e0248e5f097ade3
YgLK <kuba21316@wp.pl>
2023-08-15 16:17:32 +0200

    Add pydantic models

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -5,9 +5,15 @@ from pydantic_extra_types.phone_numbers import PhoneNumber
 
 
 class Role(str, Enum):
-    user = "user"
-    moderator = "moderator"
-    admin = "admin"
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+
+class OrderStatus(str, Enum):
+    PENDING = "pending"
+    PROCESSING = "processing"
+    DELIVERED = "delivered"
+    CANCELLED = "cancelled"
 
 
 class UserBase(BaseModel):
@@ -19,7 +25,57 @@ class UserBase(BaseModel):
     role: Optional[Role] = None
     phone_number: Optional[PhoneNumber] = None
 
-
 class User(UserBase):
+    id: int
+    created_at: PastDatetime
+
+
+class AddressBase(BaseModel):
+    user_id: int
+    street: str
+    city: str
+    state: str
+    postal_code: str
+    created_at: PastDatetime
+
+class Address(AddressBase):
+    id: int
+
+
+class OderBase(BaseModel):
+    user_id: int
+    order_status: Optional[OrderStatus] = None
+    created_at: PastDatetime
+
+class Order(OderBase):
+    id: int
+
+
+class OrderRecord(BaseModel):
+    order_id: int
+    item_id: int
+    quantity: int
+
+
+class ItemBase(BaseModel):
+    name: str
+    description: str
+    main_category: str
+    sub_category: str
+    image_url: str
+    price: float
+    stock_quantity: int
+
+class Item(ItemBase):
+    id: int
+
+
+class ReviewBase(BaseModel):
+    user_id: int
+    item_id: int
+    rating: int
+    comment: str
+
+class Review(ReviewBase):
     id: int
     created_at: PastDatetime

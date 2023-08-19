96d41667a0c9481f3ed4b0f4629416686be72648
YgLK <kuba21316@wp.pl>
2023-08-20 01:11:20 +0200

    Minor refactor

--- backend/app/database.py
+++ backend/app/database.py
@@ -18,13 +18,12 @@ DB_NAME = os.getenv("POSTGRES_DB")
 # Construct the database URL
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 # Dependency
 def get_db():

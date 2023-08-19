cc01486c6fb2355d6b380c65330990e85150453e
YgLK <kuba21316@wp.pl>
2023-08-19 16:04:51 +0200

    Minor code refactor

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 # Load environment variables from the specified .env file
-dotenv_path = Path(os.path.abspath(__file__)).parent / '../.env'
+dotenv_path = Path(__file__).resolve().parent / "../.env"
 load_dotenv(dotenv_path)
 
 # Construct the database URL
@@ -14,7 +15,6 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = os.getenv("POSTGRES_HOST")
 DB_NAME = os.getenv("POSTGRES_DB")
-
 # Construct the database URL
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,44 +1,17 @@
-from enum import Enum
+from fastapi import FastAPI
 
-from fastapi import Depends, FastAPI, HTTPException, Response, status
-from sqlalchemy.orm import Session
-
-from . import models, schemas
-from .database import engine, get_db
-from .hasher import HashGenerator
+from . import models
+from .database import engine
+from .routers import user
 
 # create tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+app.include_router(user.router)
 
-@app.get('/')
+
+@app.get("/")
 def index():
     return {"Hello": "World"}
-
- 
-@app.get('/sqlalchemy')
-async def sqlalchemy(db: Session = Depends(get_db)):
-    return {"status": "ok"}
-
-
-# USERS
-@app.post("/users/", status_code = status.HTTP_201_CREATED)
-async def create_user(user: schemas.UserBase, db: Session = Depends(get_db)) -> schemas.UserBase:
-    # hash password
-    user.password = HashGenerator.get_password_hash(user.password)
-    # create user
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    return new_user
-
-
-@app.get("/users/{user_id}", status_code = status.HTTP_200_OK)
-async def get_user(user_id: str, db: Session = Depends(get_db)) -> schemas.User:
-    # retrieve user from database
-    user = db.query(models.User).filter(models.User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    return user

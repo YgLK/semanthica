c757d639fedd71f0f460ab450ca486768c08bfaf
YgLK <kuba21316@wp.pl>
2023-09-05 23:41:59 +0200

    Add image tool to utils

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,12 @@
+from io import BytesIO
+
+import PIL.ImageShow
+import cv2
+import urllib.request
 from passlib.context import CryptContext
+import requests
+import numpy as np
+from PIL import Image
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -11,3 +19,14 @@ class PasswordHasher:
     @staticmethod
     def get_password_hash(password: str) -> str:
         return pwd_context.hash(password)
+
+
+class ImageTool:
+    @staticmethod
+    def load_image_from_url(url: str):
+        response = urllib.request.urlopen(url)
+        image_data = response.read()
+        image_array = np.asarray(bytearray(image_data), dtype=np.uint8)
+        image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image

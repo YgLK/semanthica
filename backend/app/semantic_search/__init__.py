7284b6e86d06f5e154811f9db43de8f5314c9c8a
YgLK <kuba21316@wp.pl>
2023-09-05 23:49:15 +0200

    Refactor client wrapper, add vector db collection initialization

--- backend/app/semantic_search/__init__.py
+++ backend/app/semantic_search/__init__.py
@@ -1,3 +1,4 @@
 from .search import (vector_client,
                      text_embedding_generator,
-                     image_embedding_generator)
+                     image_embedding_generator,
+                     QDRANT_COLLECTION_NAME)

00085deef8826a8b98ff9161531c5b2fda2f02d6
YgLK <kuba21316@wp.pl>
2023-10-13 00:36:16 +0200

    Add prefix to the api routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,13 +9,13 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(user.router)
-app.include_router(address.router)
-app.include_router(order.router)
-app.include_router(item.router)
-app.include_router(review.router)
-app.include_router(order_record.router)
-app.include_router(search.router)
+app.include_router(user.router, prefix="/api")
+app.include_router(address.router, prefix="/api")
+app.include_router(order.router, prefix="/api")
+app.include_router(item.router, prefix="/api")
+app.include_router(review.router, prefix="/api")
+app.include_router(order_record.router, prefix="/api")
+app.include_router(search.router, prefix="/api")
 
 
 @app.get("/")

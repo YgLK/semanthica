76687b6a8c2559ee5c0f0afaed7aea59ac1efd0c
YgLK <kuba21316@wp.pl>
2023-10-13 00:37:27 +0200

    Add item reviews get request, introduce pagination for items retrieval

--- backend/app/routers/item.py
+++ backend/app/routers/item.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status, Query
 from sqlalchemy.orm import Session
 
 from .. import models, schemas
@@ -91,5 +91,37 @@ async def delete_item(item_id: int, db: Session = Depends(get_db)):
 @router.get(
     "/", status_code=status.HTTP_200_OK, response_model=List[schemas.ItemOut]
 )
-async def get_all_items(db: Session = Depends(get_db)):
-    return db.query(models.Item).all()
+async def get_items(
+        page: Optional[int] = Query(1, description="Page number", gt=0),
+        items_per_page: Optional[int] = Query(10, description="Items per page", gt=0),
+        db: Session = Depends(get_db)
+):
+    # Calculate the offset based on the page and items_per_page values
+    offset = (page - 1) * items_per_page if page and items_per_page else 0
+
+    # Query the database with pagination
+    items = db.query(models.Item).offset(offset).limit(items_per_page).all()
+    return items
+
+
+@router.get(
+    "/{item_id}/reviews", status_code=status.HTTP_200_OK, response_model=List[schemas.ItemReviewsOut]
+)
+async def get_item_reviews(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if not item:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+    reviews = db.query(models.Review).filter(models.Review.item_id == item_id).all()
+
+    # find username for each review
+    for review in reviews:
+        user = db.query(models.User).filter(models.User.id == review.user_id).first()
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            )
+        review.username = user.username
+
+    return reviews

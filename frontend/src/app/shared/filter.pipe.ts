d0cc8e14afbc18a731ab2bac7dc554dcd880b58d
YgLK <kuba21316@wp.pl>
2023-10-15 15:28:21 +0200

    Adjust the filtering pipeline to the items

--- frontend/src/app/shared/filter.pipe.ts
+++ frontend/src/app/shared/filter.pipe.ts
@@ -1,70 +1,84 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {Dish} from "../../models/dish";
 import {Filter} from "../../models/filter";
+import {Item} from "../../models/item";
 
 @Pipe({
   name: 'filterPipe',
   pure: false
 })
 export class FilterPipe implements PipeTransform {
-  transform(dishes: Dish[], filter: Filter): Dish[] {
+  transform(items: Item[], filter: Filter): Item[] {
     // console.log('filter: ' + filter);
-    let filtered = this.filterByName(dishes, filter.filterText);
+    let filtered = this.filterByName(items, filter.filterText);
     filtered = this.filterByPrice(filtered, filter.minPrice, filter.maxPrice)
     filtered = this.filterByRating(filtered, filter.minRating);
-    filtered = this.filterByCuisine(filtered, filter.filterCuisines);
     filtered = this.filterByCategory(filtered, filter.filterCategories);
+    // filtered = this.filterBySubcategory(filtered, filter.filterCuisines);
     return filtered;
   }
 
-  filterByName(dishes: Dish[], searchText: string): Dish[] {
+  filterByName(items: Item[], searchText: string): Item[] {
     // console.log('searchText: ' + searchText);
-    if (!dishes)
+    if (!items)
       return [];
     if (!searchText)
-      return dishes;
+      return items;
     searchText = searchText.toLowerCase();
-    return dishes.filter(dish => {
-      return dish.name.toLowerCase().includes(searchText);
+    return items.filter(item => {
+      return item.name.toLowerCase().includes(searchText);
     });
   }
 
-  filterByPrice(dishes: Dish[], minPrice: number, maxPrice:number): Dish[] {
+  filterByPrice(items: Item[], minPrice: number, maxPrice:number): Item[] {
     if (minPrice > maxPrice) {
       console.log('PRICE FILTER ERROR: minPrice > maxPrice');
-      return dishes;
+      return items;
     }
-    return dishes.filter(dish => {
-      return dish.price >= minPrice && dish.price <= maxPrice;
+    return items.filter(item => {
+      return item.price >= minPrice && item.price <= maxPrice;
     });
   }
 
-  filterByRating(dishes: Dish[], minRating: number): Dish[] {
-    // console.log('minRating: ' + minRating);
-    if(minRating < 0 || minRating > 5) {
-      console.log('RATING FILTER ERROR: minRating < 0 || minRating > 5');
-      return dishes;
-    }
-    return dishes.filter(dish => {
-      return dish.getAvgRating() >= minRating || dish.getAvgRating() === -1;
-    });
+  // TODO: fix the filtering based on the rating of items
+  //    (or just create sort by rating)
+  filterByRating(items: Item[], minRating: number): Item[] {
+    // // console.log('minRating: ' + minRating);
+    // if(minRating < 0 || minRating > 5) {
+    //   console.log('RATING FILTER ERROR: minRating < 0 || minRating > 5');
+    //   return items;
+    // }
+    // return items.filter(item => {
+    //   return item.getAvgRating() >= minRating || item.getAvgRating() === -1;
+    // });
+    return items;
   }
 
-  filterByCuisine(dishes: Dish[], cuisines: string[]): Dish[] {
-    if(cuisines.length === 0) {
-      return dishes;
-    }
-    return dishes.filter(dish => {
-      return cuisines.includes(dish.cuisine);
-    });
-  }
-
-  filterByCategory(dishes: Dish[], categories: string[]): Dish[] {
+  filterByCategory(items: Item[], categories: string[]): Item[] {
     if(categories.length === 0) {
-      return dishes;
+      return items;
     }
-    return dishes.filter(dish => {
-      return categories.includes(dish.category);
+    return items.filter(item => {
+      return categories.includes(item.mainCategory);
     });
   }
+
+  // TODO: fix filtering by subcategory
+  filterBySubcategory(items: Item[], subcategories: string[]): Item[] {
+    if(subcategories.length === 0) {
+      return items;
+    }
+    return items.filter(item => {
+      return subcategories.includes(item.subCategory);
+    });
+  }
+
+  // filterByCuisine(items: Item[], cuisines: string[]): Item[] {
+  //   if(cuisines.length === 0) {
+  //     return items;
+  //   }
+  //   return items.filter(item => {
+  //     return cuisines.includes(item.cuisine);
+  //   });
+  // }
 }

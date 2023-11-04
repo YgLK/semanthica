3864c04b2c5fa1b40cef42067796cd1e1afb24c1
YgLK <kuba21316@wp.pl>
2023-11-04 03:18:40 +0100

    Update the user model on frontend

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -1,26 +1,34 @@
-import {Order} from "./order";
+import {Address} from "./address";
+
+export enum Role {
+  USER = "user",
+  MODERATOR = "moderator",
+  ADMIN = "admin"
+}
 
 export class User {
   constructor(
-    public id: number,
-    public username: string,
-    public password: string,
-    public role: string,
-    public firstName: string,
-    public lastName: string,
-    public email: string,
-    public phone: string,
-    public address: string,
-    public orderHistory: Order[]
+    public id: number = -1,
+    public username: string = "",
+    public password: string = "",
+    public role: Role = Role.USER,
+    public firstName: string = "",
+    public lastName: string = "",
+    public email: string = "",
+    public phone: string = "",
+    public addresses: Address[],
   ){}
 
-  addOrder(dishes: Map<number, number>, total: number) {
-    // let total = 0;
-    // // console.log(dishes);
-    // dishes.forEach((value, key) => {
-    //   total += key.price * value;
-    // });
-    let order = new Order(this.orderHistory.length + 1, this.id, new Date().toLocaleString(), dishes, total);
-    this.orderHistory.push(order);
+  toJSON() {
+    return {
+      username: this.username,
+      password: this.password,
+      role: this.role,
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email,
+      phone_number: this.phone,
+      addresses: this.addresses.map(address => address.toJSON())
+    };
   }
 }

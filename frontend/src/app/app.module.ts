64b593c631533e030c6127019792023fc57e6e5b
YgLK <kuba21316@wp.pl>
2023-11-22 16:17:10 +0100

    Add interceptor to inject the authorization into requests

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -37,7 +37,7 @@ import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
 // import { LoginComponent } from './login/login.component';
 // import { RegisterComponent } from './register/register.component';
-
+import { TokenInterceptorService } from "./shared/token-interceptor.service";
 
 @NgModule({
   declarations: [
@@ -78,7 +78,11 @@ import { SearchComponent } from './search/search.component';
     NgImageSliderModule,
     HttpClientModule
   ],
-  providers: [DishService],
+  providers: [DishService, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

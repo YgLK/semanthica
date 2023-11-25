b6c15d7f80f5372b6a46ea6bbfffe0873fc690bc
YgLK <kuba21316@wp.pl>
2023-11-25 23:21:48 +0100

    Correct the local storage token name in logout method

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -63,7 +63,7 @@ export class AuthService {
   }
 
   logout() {
-    localStorage.removeItem("id_token");
+    localStorage.removeItem("access_token");
     localStorage.removeItem("expires_at");
   }
 
